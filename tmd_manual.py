04ff41d3b9fc1d90b8fe105d5630ce5afa8a6df6
Emilien Ganier <emilien.ganier@laposte.net>
2023-12-19 13:14:01 +0100

    Automatic bandwidth choice

--- tmd_manual.py
+++ tmd_manual.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 from gudhi import plot_persistence_diagram, plot_persistence_barcode
 from gudhi.representations import PersistenceImage
 
+import copy
+import math
+from itertools import chain
+
+import numpy as np
+from numpy.linalg import norm
+from scipy import stats
+from scipy.spatial.distance import cdist
+
 
 def compute_tmd(tree, positions):
     """
@@ -63,15 +72,38 @@ def compute_tmd(tree, positions):
     TMD.append((v[root], 0))
     return np.array(TMD)
 
+def get_limits(phs_list):
+    """Returns the x-y coordinates limits (min, max) for a list of persistence diagrams."""
+    ph = copy.deepcopy(phs_list)
+    xlim = [min(np.transpose(ph)[0]), max(np.transpose(ph)[0])]
+    ylim = [min(np.transpose(ph)[1]), max(np.transpose(ph)[1])]
+    return xlim, ylim
 
 def get_TMD_vector(bc, reso=100, graphic=False):
     """
     compute the flatten persistence image associted to the barcode bc
     """
-    PI = PersistenceImage(bandwidth=1, resolution=[reso, reso])
-    pi = PI.fit_transform([bc])
+    # PI = PersistenceImage(bandwidth=100, resolution=[reso, reso])
+    # pi = PI.fit_transform([bc])
 
-    pi2 = np.flip(np.reshape(pi[0], [reso, reso]), 0)
+    # pi2 = np.flip(np.reshape(pi[0], [reso, reso]), 0)
+
+
+    xlim, ylim = get_limits(bc)
+    res = complex(0, reso)
+    X, Y = np.mgrid[xlim[0] : xlim[1] : res, ylim[0] : ylim[1] : res]
+
+    values = np.transpose(bc)
+    print(values.shape)
+    kernel = stats.gaussian_kde(values, bw_method=None, weights=None)
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    Z = np.reshape(kernel(positions).T, X.shape)
+
+    norm_factor = np.max(Z)
+
+    return (Z / norm_factor).flatten()
+
+
     if graphic:
         plt.imshow(pi2)
         plt.title("Persistence Image")
